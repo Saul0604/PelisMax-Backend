698a0bfa1c6cb819af9caf7ee942975a2e115682
Luis Felipe Anaya <luisfelipeanayaparada@gmail.com>
2026-04-29 22:38:40 -0600

    feat(movies): implementar sistema de valoración y promedios (HU/08)
    - Creación de la tabla/modelo Ratings (User_ID, Movie_ID, Score).- Nuevo endpoint POST /rate con lógica de upsert (crea o actualiza el voto del usuario).- Modificación del endpoint GET /movie/:id para devolver el promedio (AVG) y el conteo total de votos.

--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -1,9 +1,11 @@
 const userRouter = require('./usersRoutes');
 const movieRoutes = require('./movies');
+const ratingRoutes = require('./ratings');
 
 function routerApi(app) {
     app.use('/api/auth', userRouter);
     app.use('/api/movies', movieRoutes);
+    app.use('/api/rate', ratingRoutes);
 }
 
 module.exports = routerApi;
